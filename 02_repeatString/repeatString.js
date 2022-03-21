a66d2221775b1556b48a3b119828c131d8b1a682
jon <jonhel619@gmail.com>
2022-03-21 22:03:01 +0800

    finished first part

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    let inputWord = '';
+    for (let i = 0; i < number; i++) {
+      inputWord += word;
+    }
+    return inputWord;
 };
+
+
+repeatString("hey",3)
+
 
 // Do not edit below this line
 module.exports = repeatString;
