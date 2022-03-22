0f52411ca0863d652652fd92c44b09a77c474ca7
jon <jonhel619@gmail.com>
2022-03-22 12:36:54 +0800

    finished reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
+    for (i =  string.length - 1; i >= 0 ; i--) {
+        newString = newString + string[i];
+    }
+    return newString
 
 };
+
+reverseString("hello")
+reverseString("hello there")
+reverseString("123! abc!")
+reverseString(" ")
 
 // Do not edit below this line
 module.exports = reverseString;
