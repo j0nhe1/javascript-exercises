e3203062392394bb9a9ab30e034c5bf405da35ed
jon <jonhel619@gmail.com>
2022-03-22 09:02:12 +0800

    finished repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,21 @@
+const randomNumber = Math.floor(Math.random() * 1000)
 const repeatString = function(word, number) {
-    let inputWord = '';
+    if (number < 0) return "ERROR";
+    let inputWord = "";
     for (let i = 0; i < number; i++) {
       inputWord += word;
-    }
+      }
     return inputWord;
 };
 
 
 repeatString("hey",3)
+repeatString("hey",10)
+repeatString("hey",1)
+repeatString("hey",0)
+repeatString("hey",-1)
+repeatString("hey",randomNumber)
+repeatString("hey", )
 
 
 // Do not edit below this line
