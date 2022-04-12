16b139f487be284af9e8d8623da744b5e0151348
jon <jonhel619@gmail.com>
2022-04-12 15:00:13 +0800

    finished temp conversion!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let converted = ((fah - 32) * 5) / 9;
+  return Math.round(converted*10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let converted = ((cel * 9) / 5 ) + 32;
+  return Math.round(converted*10) / 10;
 };
+
+ftoc(32)
+ftoc(100)
+ftoc(-100)
+ctof(0)
+ctof(73.2)
+ctof(-10)
+
 
 // Do not edit below this line
 module.exports = {
