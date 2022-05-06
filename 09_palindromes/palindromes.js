45aaac561fa3babf9244d9596fe06d4a90226997
jon <jonhel619@gmail.com>
2022-05-07 06:20:50 +0800

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let char = str.toLowerCase().replace(/[\W_]/g, ""),
+        revChar = char.split("").reverse().join("");
+    return char == revChar;
 };
+
+palindromes('racecar');
+palindromes('racecar!');
+palindromes('Racecar!');
+palindromes('A car, a man, a maraca.');
+palindromes('Animal loots foliated detail of stool lamina.');
+palindromes('ZZZZ car, a man, a maracaz.');
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
