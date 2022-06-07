0fc1883de6a52684d2f37a542013f68672cfcafb
jon <jonhel619@gmail.com>
2022-06-08 06:33:45 +0800

    finished activity

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(arr) {
+    return arr.map(function book(arr){return arr.title})
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
